1ff85c3f8ce98b3e947f012af98dbe611e892a79
Robert Jonathan <rbertt.js@gmail.com>
2021-02-22 17:10:15 -0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+     <h1>App</h1>
+
+     <Button color="cyan">Botão One</Button>
+     <Button>Botão II</Button>
+     <Button color="yellow">Botão III</Button>
+    </div>
+  );
+}
+
+export default App;
